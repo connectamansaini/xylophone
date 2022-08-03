8954502cfa180abbb24ef1698067b1525b904247
connectamansaini <connect.amansaini@gmail.com>
2022-08-03 17:07:59 +0530

    removed extra dependency

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayer
 import audioplayers_darwin
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayerPlugin.register(with: registry.registrar(forPlugin: "AudioplayerPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
